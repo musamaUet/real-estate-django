f128aa1b897fcf534090356698727cd0652ff5ed
musamauet <usamaitians.gcuf@gmail.com>
2024-03-18 12:10:49 +0500

    update listings and realtors db queries in views files

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .models import Listing
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date')
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
