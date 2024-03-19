3fdfd43b5fd93b6335c34a892b3fa38f35409631
musamauet <usamaitians.gcuf@gmail.com>
2024-03-19 12:20:42 +0500

    register accounts app and register urls

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
+    'accounts.apps.AccountsConfig',
     'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
